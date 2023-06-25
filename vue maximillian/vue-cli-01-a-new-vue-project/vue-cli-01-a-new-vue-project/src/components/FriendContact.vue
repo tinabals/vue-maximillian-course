aec1a6a40547e4bbaff73f9579b9f26ed4324043
Damilola Balogun <balogundamilola.f@gmail.com>
2023-06-25 23:22:28 +0100

    started with vue-cli

---
+++ vue maximillian/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -0,0 +1,37 @@
+<template>
+
+    <li> {{ friend.name }}</li>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}  Details</button>
+    <ul v-if="detailsAreVisible">
+        <li>
+            <strong>Phone : {{ friend.phone }}</strong>
+        </li>
+        <li>
+            <strong>Email : {{ friend.email }}</strong>
+        </li>
+    </ul>
+    
+</template> 
+
+<script>
+export default {
+    data(){
+        return {
+            detailsAreVisible : false,
+            friend : {
+                
+                    id : 'manuel',
+                    name : 'Manuel Lorenz',
+                    phone : '012 345 6798',
+                    email : 'manuel@localhost.com'
+                
+            }
+        }
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible
+        }
+    }
+}
+</script>

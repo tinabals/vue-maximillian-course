2633a68ed90e54de0bbf4b6e545afbe63f3eec36
Damilola Balogun <balogundamilola.f@gmail.com>
2023-06-26 21:41:36 +0100

    validationg props and rendering props dynamically

--- vue maximillian/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
+++ vue maximillian/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/App.vue
@@ -4,7 +4,14 @@
         <h2> My Friends </h2>
        </header>
         <ul>
-           <friend-contact></friend-contact>
+           <friend-contact
+           v-for="friend in friends"
+           :key="friend.id"
+           :name ="friend.name"
+           :phone-number = "friend.phone"
+           :email-address = "friend.emailAddress"
+           :is-favorite = "true"
+           ></friend-contact>
         </ul>
     </section>
 </template>
--- vue maximillian/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue maximillian/vue-cli-01-a-new-vue-project/vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,13 +1,15 @@
 <template>
 
-    <li> {{ friend.name }}</li>
-    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}  Details</button>
+    <li> {{name }} {{ friendIsFsvorite  ? '(Favourite)' : '' }}</li>
+    <button @click="toggleFavorite">toggle favorite</button>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }}</button>
+    
     <ul v-if="detailsAreVisible">
         <li>
-            <strong>Phone : {{ friend.phone }}</strong>
+            <strong>Phone : {{ phoneNumber }}</strong>
         </li>
         <li>
-            <strong>Email : {{ friend.email }}</strong>
+            <strong>Email : {{ emailAddress }}</strong>
         </li>
     </ul>
     
@@ -15,23 +17,57 @@
 
 <script>
 export default {
+    // props: [
+    //     'name',
+    //     'phoneNumber',
+    //     'emailAddress',
+    //     'isFavorite'
+    // ],
+    props : {
+        name : {
+            type  : String,
+            required : true
+        },
+        phoneNumber : {
+            type  : String,
+            required : true
+        },
+        emailAddress: {
+            type  : String,
+            required : true
+        },
+        isFavorite: {
+            type  : String,
+            required : false,
+            default : '0',
+            validator : function(value){
+                return value === '1' || value === '0'
+            }
+        }
+    },
     data(){
         return {
             detailsAreVisible : false,
-            friend : {
-                
-                    id : 'manuel',
-                    name : 'Manuel Lorenz',
-                    phone : '012 345 6798',
-                    email : 'manuel@localhost.com'
-                
-            }
+            friendIsFsvorite : this.isFavorite
         }
     },
     methods: {
         toggleDetails(){
             this.detailsAreVisible = !this.detailsAreVisible
+        },
+        toggleFavorite(){
+            // console.log(this.friendIsFsvorite)
+            // console.log(this.isFavorite, 'isfac')
+            // if(this.friendIsFsvorite === '1'){
+            //     this.friendIsFsvorite = '0'
+            // } else{
+            //     this.friendIsFsvorite = '1'
+            // }
+             this.friendIsFsvorite = ! this.friendIsFsvorite
         }
+    },
+    mounted(){
+        console.log(this.phoneNumber, this.isFavorite)
     }
 }
 </script>

2a2900ae5e9a63fb95eb8c663e851828ba69b81b
Damilola Balogun <balogundamilola.f@gmail.com>
2023-07-03 23:04:29 +0100

    understanding vuex

--- vue maximillian/vuex-01-starting-setup/vuex-01-starting-setup/src/App.vue
+++ vue maximillian/vuex-01-starting-setup/vuex-01-starting-setup/src/App.vue
@@ -1,14 +1,40 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+   <the-counter></the-counter>
+   <final-value></final-value>
+  <button @click="addOne">
+    Add 10
+  </button>
+  <change-counter></change-counter>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter'
+import ChangeCounter from './components/ChangeCounter';
+import FinalValue from './components/FinalValue'
 
 export default {
   components: {
     BaseContainer,
-  },
+    TheCounter,
+    ChangeCounter,
+    FinalValue
+  }, 
+ 
+  methods:{
+    addOne(){
+    //  const payload = {
+    //     value : 10
+    //   }
+      // this.$store.commit('increase', payload)
+      this.$store.dispatch({
+        type: 'increase',
+         value : 10
+      })
+    }
+  }
 };
 </script>
 

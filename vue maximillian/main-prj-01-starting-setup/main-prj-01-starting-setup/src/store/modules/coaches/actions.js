0324b2fbae7d2ca44f6a988b5815f0a15f19fa5e
Damilola Balogun <balogundamilola.f@gmail.com>
2023-07-10 23:56:52 +0100

    implement firebase requests to firebase database

--- vue maximillian/main-prj-01-starting-setup/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ vue maximillian/main-prj-01-starting-setup/main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -1,13 +1,24 @@
 export default {
-    registerCoach(context, data){
+   async registerCoach(context, data){
+        const userId = context.rootGetters.userId
         const coach = {
-            id : context.rootGetters.userId,
             firstName : data.first,
             lastName : data.last,
             description : data.desc,
             areas : data.areas,
             hourlyRate : data.rate
         }
-        context.commit('registerCoach', coach)
+      const response = await fetch(`https://http-proj-91e04-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+            method : 'PUT', //this updates an id or creates...post just creates everytime tho it exists or not
+            body : JSON.stringify(coach)
+        })
+        // const responseData = await response.json()
+        if(!response.ok){
+            //error
+        }
+        context.commit('registerCoach', {
+            id : userId,
+            ...coach
+        })
     }
 }

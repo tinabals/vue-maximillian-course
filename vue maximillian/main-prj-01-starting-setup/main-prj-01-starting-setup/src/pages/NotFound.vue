c7d0709fe69be34f3dfd53f4e76cdfb91382d228
Damilola Balogun <balogundamilola.f@gmail.com>
2023-07-11 22:39:58 +0100

    implemented not found page

--- vue maximillian/main-prj-01-starting-setup/main-prj-01-starting-setup/src/pages/NotFound.vue
+++ vue maximillian/main-prj-01-starting-setup/main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <section>
+    <base-card>
+    <h2>Page Not Found</h2>
+    <p> This page Could not be found, Please check out all our <router-link to='/coaches'>coaches</router-link></p>
+  </base-card>
+  </section>
+
 </template>
 
 <script>
